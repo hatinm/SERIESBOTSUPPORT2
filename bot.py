3daeb2c5adb4516c6c70d9532bf65ac6bc82dca3
hatinm <48410878+hatinm@users.noreply.github.com>
2019-03-17 20:22:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
+import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import requests
+import os
 
 Client = discord.client
 client = commands.Bot(command_prefix = '!')
